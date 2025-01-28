0c1016e0f51a39e967429fafb97f51f0c6ee8c0e
Mar <mar.melich@gmail.com>
2025-01-28 19:05:49 +0100

    28 enero 3

--- www/js/index.js
+++ www/js/index.js
@@ -6,26 +6,33 @@ function onDeviceReady() {
     // console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     // document.getElementById('deviceready').classList.add('ready');
     document.getElementById("btnCapture").addEventListener("click", function () {
-      navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-        quality: 100,
-        targetWidth: 1000,
-        targetHeight: 1330,
-        destinationType: Camera.DestinationType.NATIVE_URI,
-        saveToPhotoAlbum: true,
-        correctOrientation: true
-        });
-        // also tested with FILE_URI, and all possible setting for destinationType, and it does return the correct file path, but the file path isnt allowed to be displayed....and showing the not allowed to load local resource error...does it have something to do with Google changing the location for secure storage?
-        
-        function onPhotoDataSuccess(imageURI) {
-        console.log(imageURI);
-        var cameraImage = document.getElementById('image');
-        cameraImage.src = imageURI;
-        }
-        
-        function onFail(message) {
-        console.log(message);
-        }
-      })
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 50, // Calidad de la imagen (0-100)
+        destinationType: Camera.DestinationType.DATA_URL, // Devuelve Base64
+        sourceType: Camera.PictureSourceType.CAMERA // Captura desde la cámara
+    });
+
+    function onSuccess(imageData) {
+        // imageData contiene el string Base64 de la imagen
+        var base64Image = imageData;
+        console.log(base64Image);
+
+        // Guarda en localStorage
+        localStorage.setItem("savedImage", base64Image);
+
+        // Muestra la imagen guardada
+        var image = document.getElementById("image");
+        image.src = base64Image;
+        image.style.display = "block";
+        document.getElementById("userPhoto").src = base64Image;
+
+        alert("Imagen guardada en localStorage");
+    }
+
+    function onFail(message) {
+        alert("Error al capturar la imagen: " + message);
+    }
+});
 }
 
 window.onload = (event) => {
@@ -33,10 +40,9 @@ window.onload = (event) => {
     var options = { "swipeable": true };
     var el = document.getElementsByClassName('tabs');
     var instance = M.Tabs.init(el, options);
-   
+
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {
       // specify options here
     });
 };
-
