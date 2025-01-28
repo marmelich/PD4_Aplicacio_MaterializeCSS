34cff0cd900dbb99afbd634e59cd0a84b5d3b40b
Mar <mar.melich@gmail.com>
2025-01-28 18:33:48 +0100

    28 enero 2

--- www/js/index.js
+++ www/js/index.js
@@ -5,6 +5,27 @@ function onDeviceReady() {
 
     // console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     // document.getElementById('deviceready').classList.add('ready');
+    document.getElementById("btnCapture").addEventListener("click", function () {
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+        quality: 100,
+        targetWidth: 1000,
+        targetHeight: 1330,
+        destinationType: Camera.DestinationType.NATIVE_URI,
+        saveToPhotoAlbum: true,
+        correctOrientation: true
+        });
+        // also tested with FILE_URI, and all possible setting for destinationType, and it does return the correct file path, but the file path isnt allowed to be displayed....and showing the not allowed to load local resource error...does it have something to do with Google changing the location for secure storage?
+        
+        function onPhotoDataSuccess(imageURI) {
+        console.log(imageURI);
+        var cameraImage = document.getElementById('image');
+        cameraImage.src = imageURI;
+        }
+        
+        function onFail(message) {
+        console.log(message);
+        }
+      })
 }
 
 window.onload = (event) => {
@@ -18,3 +39,4 @@ window.onload = (event) => {
       // specify options here
     });
 };
+
